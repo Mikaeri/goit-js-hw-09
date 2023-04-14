b0d5e6c6e8ba03935655d4a04718c0b41600ce7d
Микола Іванишин <43409421+Mikaeri@users.noreply.github.com>
2023-04-14 01:24:51 +0100

    task-1 (completed)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const startBtn = document.querySelector("[data-start]");
+const stopBtn = document.querySelector("[data-stop]");
+const docBody = document.querySelector("body")
+let isClicked = false
+let intervalId = null;
 
+startBtn.addEventListener('click', () => {
+if(!isClicked){
+    intervalId = setInterval (() =>{
+        docBody.style.backgroundColor = getRandomHexColor();
+        }, 1000);
+    isClicked = true;
+}});
+
+stopBtn.addEventListener('click', () =>{
+    clearInterval(intervalId);
+    isClicked = false;
+    docBody.style.backgroundColor = `#fafafa`;
+});
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
